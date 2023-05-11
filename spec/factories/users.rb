120775924c4fc9c26e678811a6355dafc52890c3
massa1111 <t09071460846@gmail.com>
2023-05-11 20:50:16 +0900

    単体テストコード新規登録できる場合

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 4)}
-    last_name             {Faker::Name.initials(number: 4)}
-    first_name            {Faker::Name.initials(number: 4)}
-    last_name_kana        {Faker::Name.initials(number: 4)}
-    first_name_kana       {Faker::Name.initials(number: 4)}
+    last_name             {'山田'}
+    first_name            {'陸太'}
+    last_name_kana        {'ヤマダ'}
+    first_name_kana       {'リクタ'}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
-
+      it "nickname、last_name、first_name、last_name_kana、first_name_kana、birth_dateとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
-      it "nameが空では登録できない" do
+      it "nicknameが空では登録できない" do
+
+      end
+      it "last_nameが空では登録できない" do
+
+      end
+      it "first_nameが空では登録できない" do
+
+      end
+      it "last_name_kanaが空では登録できない" do
+
+      end
+      it "first_name_kanaが空では登録できない" do
+
+      end
+      it "last_name_kanaが空では登録できない" do
+
+      end
+      it "first_name_kanaが空では登録できない" do
+
+      end
+      it "birth_dateが空では登録できない" do
 
       end
       it "emailが空では登録できない" do
@@ -18,6 +44,12 @@ RSpec.describe User, type: :model do
       it "passwordが空では登録できない" do
 
       end
+      it "password_confirmationが空では登録できない" do
+
+      end
+     
+      
+      
       it 'passwordが5文字以下では登録できない' do
 
       end
