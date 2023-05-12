dc6f3e16fb3af6a36c21677dc997d69856181f7a
massa1111 <t09071460846@gmail.com>
2023-05-12 11:45:51 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 4)}
-    last_name             {'山田'}
-    first_name            {'陸太'}
-    last_name_kana        {'ヤマダ'}
-    first_name_kana       {'リクタ'}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.initials(number: 4) }
+    last_name             { '山田' }
+    first_name            { '陸太' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'リクタ' }
+    email                 { Faker::Internet.free_email }
     password              { '1a' + Faker::Internet.password(min_length: 6) }
-    password_confirmation {password}
-    birth_date            {Faker::Date.birthday(min_age: 4, max_age: 80)}
+    password_confirmation { password }
+    birth_date            { Faker::Date.birthday(min_age: 4, max_age: 80) }
   end
 end
-
