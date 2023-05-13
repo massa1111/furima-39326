1d681a37deb5ed08b61fb6590b85dabce6c87320
massa1111 <t09071460846@gmail.com>
2023-05-13 14:15:42 +0900

    ログイン別商品出品ページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @items = Item.includes(:user)
   end
@@ -8,6 +9,7 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  
   def create
     @item = Item.new(item_params)
     if @item.save
