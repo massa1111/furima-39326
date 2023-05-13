42754c1fc1f6fdea215a0ee8cdc800fc0e90282f
massa1111 <t09071460846@gmail.com>
2023-05-13 12:26:09 +0900

    空バリデーツ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
   validates :category_id, numericality: { other_than: 1 } 
+  validates :sales_status_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }  
+  validates :price, numericality: { other_than: 1 } 
+
 
 end
 
