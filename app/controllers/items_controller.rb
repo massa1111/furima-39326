88689209a702ee4a75f14710c0e2d96ca89a501f
massa1111 <t09071460846@gmail.com>
2023-05-13 12:13:40 +0900

    商品情報データベース保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,28 @@
 class ItemsController < ApplicationController
   def index
-   
+    @items = Item.includes(:user)
   end
 
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+ 
 
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+  params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,:price, :image).merge(user_id: current_user.id)
   end
 
 end
