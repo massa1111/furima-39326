d94d73be80e3bc315999bd6cb9e48a72ec636595
massa1111 <t09071460846@gmail.com>
2023-05-14 21:18:09 +0900

    テストコード完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   { 'Tシャツ' }
+    info                   { '目立ったキズ無し' }
+    category_id            { 2 }
+    sales_status_id        { 2 }
+    shipping_fee_status_id { 2 }
+    prefecture_id          { 2 }
+    scheduled_delivery_id  { 2 }
+    price                  { '9999999'}
+  association :user 
+
+  after(:build) do |item|
+    item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
   end
+  end
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品できる場合' do
+      it 'name、info、category_id、sales_status_id、
+      shipping_fee_status_id、prefecture_idとscheduled_delivery_id、price、imageが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない場合' do
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空では登録できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      
+      it 'category_idが空では登録できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+      
+      it 'sales_status_idが空では登録できない' do
+        @item.sales_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status is not a number")
+      end
+      it 'shipping_fee_status_idが空では登録できない' do
+        @item.shipping_fee_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status is not a number")
+      end
+      
+      it 'prefecture_idが空では登録できない' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      end
+      
+      it 'scheduled_delivery_idが空では登録できない' do
+        @item.scheduled_delivery_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery is not a number")
+      end
+      
+      it 'priceが空では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      
+      it 'priceが半角数字以外では登録できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      
+      it 'priceが¥299以下では登録できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      
+      it 'priceが¥10,000,000以下では登録できない' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      
+      
+      
+    end
+  end
 end
