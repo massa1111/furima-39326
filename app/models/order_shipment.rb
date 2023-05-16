02483f5d9ed343de45bcade5647dd4908bae4604
massa1111 <t09071460846@gmail.com>
2023-05-16 17:39:01 +0900

    order_shipment修正

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -13,8 +13,15 @@ class OrderShipment
   end
   validates :prefecture_id, numericality: { other_than: 1 }
   def save
-   order = Order.create(item: item, user_id: user_id)
-    
-   Shipment.create(postal_code: postal_code, city: city, addresses: addresses, building: building, :phone_number: :phone_number, order_id: order.id)
+    order = Order.create(item: item, user: user)
+
+    Shipment.create(
+      postal_code: postal_code,
+      city: city,
+      addresses: addresses,
+      building: building,
+      phone_number: phone_number,
+      order_id: order.id
+    )
   end
 end
