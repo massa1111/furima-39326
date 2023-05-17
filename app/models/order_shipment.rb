d3bb30a365212b89d16da90e9745038f59a3e0f5
massa1111 <t09071460846@gmail.com>
2023-05-17 11:13:23 +0900

    OrderShipmentモデル修正

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -1,24 +1,29 @@
 class OrderShipment
   include ActiveModel::Model
-  attr_accessor :item, :user, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order
+  
+  
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order
+  
+ 
+ 
+ 
   with_options presence: true do
-   validates :item
-   validates :user
+   validates :item_id
+   validates :user_id
    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
    validates :city
    validates :addresses
-   validates :building
    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-   validates :order
   end
-  validates :prefecture_id, numericality: { other_than: 1 }
+   validates :prefecture_id, numericality: { other_than: 1 }
   def save
-    order = Order.create(item: item, user: user)
+    order = Order.create(item_id: item_id, user_id: user_id)
 
     Shipment.create(
       postal_code: postal_code,
       city: city,
       addresses: addresses,
+      prefecture_id: prefecture_id,
       building: building,
       phone_number: phone_number,
       order_id: order.id
