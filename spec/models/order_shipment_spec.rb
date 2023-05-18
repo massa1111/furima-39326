68a3c13e775f5c37a30a874e2cfa2e600ba686f2
massa1111 <t09071460846@gmail.com>
2023-05-18 17:13:47 +0900

    テストコードtoken

--- spec/models/order_shipment_spec.rb
+++ spec/models/order_shipment_spec.rb
@@ -76,6 +76,12 @@ RSpec.describe OrderShipment, type: :model do
     @order_shipment.valid?
     expect(@order_shipment.errors.full_messages).to include("User can't be blank")
   end
+
+  it "tokenが空では登録できないこと" do
+    @order_shipment.token = nil
+    @order_shipment.valid?
+    expect(@order_shipment.errors.full_messages).to include("Token can't be blank")
+  end
     end
   end
 end
