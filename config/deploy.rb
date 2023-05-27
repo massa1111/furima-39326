ab114d0e6925b8697d06cfc9d6c1d9c04aa31636
massa1111 <t09071460846@gmail.com>
2023-05-27 18:22:46 +0900

    Capfile修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,11 @@
 # config valid for current version and patch releases of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.17.3'
 
+# Capistranoのログの表示に利用する
+set :application, 'furima-39326'
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:massa1111/furima-39326.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -34,14 +40,6 @@
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.17.3'
-
-# Capistranoのログの表示に利用する
-set :application, 'furima-39326'
-
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:massa1111/furima-39326.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
